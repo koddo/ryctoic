92c63a7d233e2730a469f762b02ed809a8b61690
Alex Scherbanov <alex@egotv.ru>
2015-09-01 19:48:41 +0300

    remove compiled trash from the repo

--- priv/static/js/compiled/out/reagent/interop.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.7.48 {}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
-
-//# sourceMappingURL=interop.js.map?rel=1440504628703
--- priv/static/js/compiled/out/ryctoic/async_error_handling_helpers.cljs
+++
@@ -1,8 +0,0 @@
-(ns ryctoic.async-error-handling-helpers)
-
-(defn error? [x]
-  (instance? js/Error x))
-
-(defn throw-err [e]
-  (when (error? e) (throw e))
-  e)
